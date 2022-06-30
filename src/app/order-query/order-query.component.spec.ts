b40a9b19c5e43e52617db537a488ea10fdfa244c
Christian Cano <christian.cano@fouriering.com>
2022-06-30 11:17:04 -0500

    components login and order-query created

---
+++ src/app/order-query/order-query.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrderQueryComponent } from './order-query.component';
+
+describe('OrderQueryComponent', () => {
+  let component: OrderQueryComponent;
+  let fixture: ComponentFixture<OrderQueryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrderQueryComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OrderQueryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
