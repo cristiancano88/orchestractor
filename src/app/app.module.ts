0795a4cb787fadf5f9efc46c2197278d1f838da0
Christian Cano <christian.cano@fouriering.com>
2022-06-30 13:31:38 -0500

    login html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OrderQueryComponent } from './order-query/order-query.component';
 import { LoginComponent } from './login/login.component';
+import { OrderQueryComponent } from './order-query/order-query.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
